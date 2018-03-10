5b447332e0dee193fa98d59bf142cea79fd9b689
Luis Rincon <rincon_92@hotmail.com>
2018-03-10 17:38:09 -0500

    chat functionality works

--- src/Messenger.js
+++ src/Messenger.js
@@ -10,19 +10,30 @@ export default class Messenger extends Component {
         this.handleSubmitMessage = this.handleSubmitMessage.bind(this);
     }
 
+    componentDidMount() {
+        const messaages = firebase.database().ref('chat')
+        messaages.on('child_added', snap => {
+            const message = snap.val().text
+            console.log(message)
+            const chatText = document.createElement('li')
+            document.getElementById('chatbox').appendChild(chatText).innerHTML = message
+        })
+    }
+
     handleSubmitMessage(event) {
         event.preventDefault();
         firebase.database().ref('chat').push({
             text: event.target.text.value
         })
         event.target.text.value = '';
+        
     }
 
     render() {
         return (
             <div id="chatroom">
-                <div id="chatbox">
-                </div>
+                <ul id="chatbox">
+                </ul>
                     <form className="input" onSubmit={this.handleSubmitMessage} >
                         <input type="text" id="message" name="text" placeholder="Send a message" />
                         <input type="submit" value="Submit" />
