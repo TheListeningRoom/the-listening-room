a1f346b111f81f33262068f87901881b8a4d72d5
Noelle Laureano <nrlaureano@gmail.com>
2018-03-10 20:11:59 -0500

    message order fixed and timestamps added

--- src/Messenger.js
+++ src/Messenger.js
@@ -13,31 +13,74 @@ export default class Messenger extends Component {
     componentDidMount() {
         const messaages = firebase.database().ref('chat')
         messaages.on('child_added', snap => {
-            const message = snap.val().text
-            console.log(message)
-            const chatText = document.createElement('li')
-            document.getElementById('chatbox').appendChild(chatText).innerHTML = message
+            const message = `${snap.val().createdAt}\u00A0\u00A0\u00A0\u00A0 ${snap.val().text}`;
+            const chatText = document.createElement('div');
+            const chatBoxDiv = document.getElementById('chatbox');
+            const firstChild = chatBoxDiv.firstChild;
+            chatBoxDiv.insertBefore(chatText, firstChild).innerHTML = message
         })
     }
 
+    // loadMessages = () => {
+    //     // Remove all previous listeners:
+    //     firebase.database().ref('chat').off();
+
+    //     // Loads the last 20 messages and listens for new ones:
+    //     // const setMessage = data => {
+    //     //     let val = data.val();
+    //     //     this.displayMessage(data.key, val.text)
+    //     // }
+    //     console.log('in loadMessages')
+    //     firebase.database().ref('chat').limitToLast(5).on('child_added', snap => {
+    //         console.log(snap.val());
+    //     })
+    //     //, setMessage);
+    //     firebase.database().ref('chat').limitToLast(5).on('child_changed', snap => {
+    //         console.log(snap.val());
+    //     })
+    //     //, setMessage);
+    // }
+
+    // displayMessage = (key, text) => {
+    //     let container = document.createElement('div');
+    //     container.innerHTML = (
+    //         '<div className="message" />'
+    //     )
+    // }
+
     handleSubmitMessage(event) {
         event.preventDefault();
-        firebase.database().ref('chat').push({
-            text: event.target.text.value
-        })
-        event.target.text.value = '';
-        
+        if (event.target.text.value) {
+            firebase.database().ref('chat').push({
+                createdAt: new Date().toString().slice(0, 24),
+                text: event.target.text.value
+            })
+            .catch( error => console.error('Error writing to Firebase DB', error));
+            event.target.text.value = '';
+        }
     }
 
     render() {
         return (
             <div id="chatroom">
-                <ul id="chatbox">
-                </ul>
-                    <form className="input" onSubmit={this.handleSubmitMessage} >
-                        <input type="text" id="message" name="text" placeholder="Send a message" />
-                        <input type="submit" value="Submit" />
-                    </form>
+
+                <form className="input" onSubmit={this.handleSubmitMessage} >
+                    <input
+                        type="text"
+                        id="message"
+                        name="text"
+                        placeholder="Send a message"
+                    />
+                    <input
+                        type="submit"
+                        value="submit"
+                    />
+                </form>
+
+                <div className="historical-messages">
+                    <ul id="chatbox" />
+                </div>
+
             </div>
         )
     }
