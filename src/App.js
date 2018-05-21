25b83ebf94fc09f55cfd582cf23378bfb8c697de
Noelle Laureano <nrlaureano@gmail.com>
2018-05-21 18:12:02 -0400

    cleans up code (removes comments and console logs and prettifies)

--- src/App.js
+++ src/App.js
@@ -1,122 +1,78 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import * as firebase from 'firebase';
-import Messenger from './Messenger';
-// import Cover from './Cover';
+import React, { Component } from 'react'
+import './App.css'
+import * as firebase from 'firebase'
+import Messenger from './Messenger'
 
 class App extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       song: '',
-      songName: ''
+      songName: '',
     }
-    // this.handleSubmit = this.handleSubmit.bind(this);
     this.handleUpload = this.handleUpload.bind(this)
-    this.handlePlayPauseClick = this.handlePlayPauseClick.bind(this);
+    this.handlePlayPauseClick = this.handlePlayPauseClick.bind(this)
   }
 
   componentDidMount() {
-    console.log('in componenet did mount')
-    const messageRef = firebase.database().ref('message');
+    const messageRef = firebase.database().ref('message')
 
     messageRef.on('child_added', snap => {
       this.setState({
         song: snap.val().song,
-        songName: snap.val().songName
-      });
+        songName: snap.val().songName,
+      })
     })
-    firebase.database().ref('paused').on('value', snap => {
-      this.paused = snap.val()
-      this.setAudioState()
-    })
+    firebase
+      .database()
+      .ref('paused')
+      .on('value', snap => {
+        this.paused = snap.val()
+        this.setAudioState()
+      })
   }
 
   setAudioState = () => {
-    console.log('******** In setAudioState *********')
-    console.log('this.paused -->', this.paused)
-    console.log('this.audio -->', this.audio)
     if (this.paused) {
-      this.audio.pause();
+      this.audio.pause()
     } else {
-      this.audio.play();
+      this.audio.play()
     }
   }
 
   audioDidMount = audio => {
-    console.log('******** In audioDidMount *********')
-    console.log('audioDidMount audio -->', audio)
-    this.audio = audio;
+    this.audio = audio
     this.audio.addEventListener('loadeddata', this.setAudioState)
   }
 
   handlePlayPauseClick(event) {
-    event.preventDefault();
-
-    const pauseRef = firebase.database().ref('paused');
+    event.preventDefault()
+    const pauseRef = firebase.database().ref('paused')
     pauseRef.set(!this.audio.paused)
-
-    // let play = document.getElementById('audio');
-
-    // if (document.getElementById('play')) {
-    //   play.pause();
-    //   document.getElementById('play').id = 'pause';
-    //   playRef.set(false);
-    // } else {
-    //   document.getElementById('pause').id = 'play';
-    //   play.play();
-    //   playRef.set(true);
-    // }
-    // playRef.on('value', snap => {
-    //   console.log(snap.val())
-    //   if (snap.val) {
-    //     play.pause()
-    //   } else {
-    //     play.play()
-    //   }
-    // })
   }
-
-  // handleSubmit(event) {
-  //   event.preventDefault();
-  //   const messageRef = firebase.database().ref('message');
-  //   messageRef.push({
-  //     song: event.target.song.value
-  //   }).key
-  //   event.target.song.value = '';
-  // }
 
   handleUpload(evt) {
     let file = evt.target.files[0]
-    console.log(evt.target.files)
-
     let musicRef = firebase.storage().ref('music/' + file.name)
-
-    musicRef.put(file)
-    .then(() => {
-      console.log('i hit this part 1')
+    musicRef.put(file).then(() => {
       const storageRef = firebase.storage().ref('/music')
-      storageRef.child(file.name).getMetadata()
-    .then(metaData => {
-      console.log(metaData)
-      let url = metaData.downloadURLs[0]
-      console.log(url)
-      const messageRef = firebase.database().ref('message');
-      messageRef.push({
-        song: url,
-        songName: file.name
-      })
+      storageRef
+        .child(file.name)
+        .getMetadata()
+        .then(metaData => {
+          let url = metaData.downloadURLs[0]
+          const messageRef = firebase.database().ref('message')
+          messageRef.push({
+            song: url,
+            songName: file.name,
+          })
+        })
     })
-  })
-}
-
+  }
 
   render() {
-
-    const song = this.state.song;
-    let songName;
+    const song = this.state.song
+    let songName
     if (this.state.songName) {
       songName = this.state.songName.slice(0, -4)
     }
@@ -145,14 +101,12 @@ class App extends Component {
             ▶ ❚❚
           </button>
 
-
-
           <audio src={song} ref={this.audioDidMount} loop />
 
           <Messenger />
 
         </div>
-      );
+      )
     } else {
       return (
         <div>
@@ -163,9 +117,4 @@ class App extends Component {
   }
 }
 
-export default App;
-
-// <header className="App-header">
-//   <img src={logo} className="App-logo" alt="logo" />
-//   <h1 className="App-title">The Listening Room</h1>
-// </header>
+export default App
--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import * as firebase from 'firebase';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import * as firebase from 'firebase'
 
-var config = {
-  apiKey: "AIzaSyC4IEv77ynFo0v1Tc95dz8c_w0WbUWO8Ng",
-  authDomain: "symbalplayer.firebaseapp.com",
-  databaseURL: "https://symbalplayer.firebaseio.com",
-  projectId: "symbalplayer",
-  storageBucket: "symbalplayer.appspot.com",
-  messagingSenderId: "511569276210"
-};
-firebase.initializeApp(config);
+const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY
 
-ReactDOM.render(
-  <App />, document.getElementById('root')
-);
-registerServiceWorker();
+const config = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: 'symbalplayer.firebaseapp.com',
+  databaseURL: 'https://symbalplayer.firebaseio.com',
+  projectId: 'symbalplayer',
+  storageBucket: 'symbalplayer.appspot.com',
+  messagingSenderId: '511569276210',
+}
+
+firebase.initializeApp(config)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
